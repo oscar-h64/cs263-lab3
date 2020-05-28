1a28782b7c4bb458fdce986e4372851346a6266c
Michael Gale <m.gale@warwick.ac.uk>
2020-05-28 19:31:47 +0100

    Add key negotiation with the server

--- listener/src/main/java/ServerConnection.java
+++ listener/src/main/java/ServerConnection.java
@@ -2,6 +2,7 @@ package listener;
 
 import java.net.*;
 import java.io.*;
+import java.math.BigInteger;
 
 public class ServerConnection implements Runnable {
     private Socket socket;
@@ -28,6 +29,28 @@ public class ServerConnection implements Runnable {
             this.inFromServer =
                 new BufferedReader(
                     new InputStreamReader(this.socket.getInputStream()));
+
+            // retrieve the two prime numbers from the server
+            // we use BigIntegers since the calculations we perform
+            // later may exceed 
+            BigInteger p = new BigInteger(inFromServer.readLine());
+            BigInteger g = new BigInteger(inFromServer.readLine());
+
+            // our secret: this value is arbitrary 
+            int mySecret = 1; 
+
+            // calculate the message to send
+            BigInteger powed = g.pow(mySecret);
+            BigInteger msg = powed.mod(p);
+    
+            // send the secret to the server
+            outToServer.println(msg);
+
+            // read the server's message and compute the key from it
+            // k is the shared key
+            BigInteger rmsg = new BigInteger(inFromServer.readLine());
+            BigInteger powed_k = rmsg.pow(mySecret);
+            BigInteger k = powed_k.mod(p); 
 
             String inputLine;
             while ((inputLine = inFromServer.readLine()) != null) {
