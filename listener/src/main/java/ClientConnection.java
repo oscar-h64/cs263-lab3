d0991bb350b915690edc3b54b128fa3e05e1dfe1
Michael Gale <m.gale@warwick.ac.uk>
2020-05-28 19:39:05 +0100

    Add key negotiation with client

--- listener/src/main/java/ClientConnection.java
+++ listener/src/main/java/ClientConnection.java
@@ -2,6 +2,7 @@ package listener;
 
 import java.net.*;
 import java.io.*;
+import java.math.BigInteger;
 
 public class ClientConnection implements Runnable {
     private Socket socket;
@@ -25,6 +26,32 @@ public class ClientConnection implements Runnable {
             this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 
             this.serverConnection.setOutToClient(this.out);
+
+            // our secret: this value is arbitrary 
+            int mySecret = 1; 
+
+            // choose two prime numbers (arbitrary) and send them 
+            // to the client, although normally they should be
+            // large and randomly chosen 
+            BigInteger p = BigInteger.valueOf(Primes.PRIMES[0]);
+            BigInteger g = BigInteger.valueOf(Primes.PRIMES[1]);
+
+            out.println(p);
+            out.println(g);
+
+            // retrieve a message from the client
+            BigInteger rmsg = new BigInteger(in.readLine());
+
+            // derive the key from the client's message and our
+            // secret
+            BigInteger powed_k = rmsg.pow(mySecret);
+            BigInteger k = powed_k.mod(p);
+
+            // derive a message to send to the client from our
+            // secret at the primary number; then send it to the client 
+            BigInteger powed = g.pow(mySecret);
+            BigInteger msg = powed.mod(p);
+            out.println(msg);
 
             // read messages and try to the pass them on to the actual server
             String inputLine;
